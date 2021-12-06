a43ec1eeb367e9b8dc705ec13e1197e6c8e5d030
illinois <boowoa@beaa.com>
2021-12-06 17:42:35 +0700

    fixAdminpanel

--- AdminPanel.php
+++ AdminPanel.php
@@ -5,10 +5,15 @@ $twig = new \Twig\Environment($loader);
 $template = $twig->loadTemplate('AdminPanel.html');
 
 
-
+include('ConnectDB.php');
+$genre = [];
+$result_genr = $SQLCONN->query("select * from genre");
+while ($r = mysqli_fetch_object($result_genr)){
+    $genre[]=$r;
+}
 $Session = (isset($_COOKIE['RequestSessionIWP']) and $_COOKIE['RequestSessionIWP'] != '');
 
-echo $template->render(array('session' => $Session));
+echo $template->render(array('session' => $Session, 'genres' => $genre));
 
 
 ?>
